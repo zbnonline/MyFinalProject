621e9caf1fad0ecc37506f80e438706e561a8f24
anıl <nbr-nb@hotmail.com>
2023-07-03 11:32:33 +0300

    Ef Geçiş

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //Generic constraint: T değişkenini kısıtlamaya verilen ifade
+    // class : referanst tip olabilir anlamına gelmektedir
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    // new : new' lenebilir olmalı
+    public interface IEntityRepository<T> where T: class,  IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
